946d7518ad23fbc8ba1aca6f6d887c07118a83a4
Anna <admin@lensesrequired.com>
2020-07-21 19:02:28 -0500

    Update Header.js

--- home/components/Header.js
+++ home/components/Header.js
@@ -3,5 +3,6 @@ import React from 'react';
 export default () => (
   <div>
     <h2>LensesRequired: UNDER CONSTRUCTION</h2>
+    <h3>Anna Marek Portfolio (if Stephen sent you here, it's because he knows I haven't had any time to work on it and he wants to be homeless)</h3>
   </div>
 )
