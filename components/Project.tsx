dcd67e585492deaceca907ee5fc39d4c7d50db2f
Anna <admin@lensesrequired.com>
2025-04-01 12:58:14 -0600

    fix types

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { Badge, Col, Row } from 'react-bootstrap';
 import { AiFillGithub } from 'react-icons/ai';
 
-export default ({ name, link, description, git, src }) => (
+type ProjectProps = {
+  name: string;
+  link: string;
+  description: string;
+  git: string;
+  src: string;
+};
+
+export default ({ name, link, description, git, src }: ProjectProps) => (
   <Row className="project">
     <Col className="image">
       <iframe src={src} height={200} width={350} />
     </Col>
     <Col>
       <a href={link}>{name}</a>
-      <Badge variant="light">
+      <Badge bg="light">
         <a href={git}>
           <AiFillGithub size={'1.25em'} /> GitHub
         </a>
